7da605d6caaa95425bd4bb1fe1372d8764f5fc42
Roberto-Livi <robertomlivi@outlook.com>
2020-12-13 11:55:47 -0500

    went over interrupting and joining a thread

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,11 +16,18 @@ public class Main {
         Thread myRunnableThread = new Thread(new MyRunnable() {
             @Override
             public void run() {
-                System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run");
+                System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run()");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED + " Another thread terminated, or timed out, so I'm running again");
+                } catch(InterruptedException e) {
+                    System.out.println(ANSI_RED + " I couldn't wait after all. I was interrupted.");
+                }
             }
         });
 
         myRunnableThread.start();
+//        anotherThread.interrupt();
         System.out.println(ANSI_PURPLE + "Hello again from the main thread");
     }
 }
